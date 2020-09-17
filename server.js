84058480b399ebd2b59f0ecd0cc532fdf86de403
Neeraj Patil <patilneeraj2212@gmail.com>
2020-09-18 00:29:56 +0530

    TEmplating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,15 +18,29 @@ app.get('/', function (req, res) {
 	};
 	let day = today.toLocaleDateString('en-US', options);
 	res.render('index', {
-		currentDay: day,
+		listTitle: day,
 		newItems: items,
 	});
 });
 
 app.post('/', function (req, res) {
 	let newItem = req.body.newItems;
-	items.push(newItem);
-	res.redirect('/');
+
+	if (req.body.list === 'Work') {
+		workItems.push(newItem);
+		res.redirect('/work');
+	} else {
+		items.push(newItem);
+		res.redirect('/');
+	}
+});
+
+app.get('/work', function (req, res) {
+	res.render('index', { listTitle: 'Work', newItems: workItems });
+});
+
+app.get('/about', function (req, res) {
+	res.render('about');
 });
 
 app.listen(3000, function () {
